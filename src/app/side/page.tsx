d6af40ea374bc59028aa1b847b908e0defa32b50
Shelden Shi <sheldenshi@gmail.com>
2024-09-02 12:16:37 -0700

    menu

---
+++ src/app/side/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+	return (
+		<main className="flex min-h-screen items-center justify-center">
+			<Image src="/2.jpg" alt="Image 1" width={500} height={300} priority />
+		</main>
+	);
+}
