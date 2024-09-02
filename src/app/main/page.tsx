79bc0d42e990419fae72f1e98786e9f30bc59880
Shelden Shi <sheldenshi@gmail.com>
2024-09-02 12:49:11 -0700

    pdf

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image';
-
 export default function Home() {
 	return (
-		<main className="flex min-h-screen items-center justify-center">
-			<Image src="/1.jpg" alt="Image 1" width={500} height={300} priority />
+		<main className="flex h-screen w-full">
+			<iframe
+				src="/main.pdf"
+				className="w-full h-full"
+				style={{ border: 'none' }}
+			/>
 		</main>
 	);
 }
